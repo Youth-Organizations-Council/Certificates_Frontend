ff017a1412454029e85cdb8c9c171de68be6056e
Arpan Pandey <arpan@hackersreboot.tech>
2022-12-30 13:04:19 +0530

    Netlify Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
-    output: 'server',
+  integrations: [mdx(), tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
